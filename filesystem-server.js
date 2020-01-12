acb3290917f98afb96862faf5340e75ef097dacf
Tsulatsi Tamim <tsulatsitamim@gmail.com>
2020-01-12 10:40:30 +0700

    change 0 day to 1 day

--- filesystem-server.js
+++ filesystem-server.js
@@ -737,7 +737,7 @@ function getPermission(filepath, name, isFile, contentRootPath, filterPath) {
 
             if (accessRule.role == accessDetails.role) {
                 const stat = fs.statSync(filepath);
-                if (((new Date().getTime() - new Date(stat.mtime).getTime()) / (24*60*60*1000)) > 0) {
+                if (((new Date().getTime() - new Date(stat.mtime).getTime()) / (24*60*60*1000)) > 1) {
                     filePermission.write = false;
                     if (isDelete) {
                         filePermission.message = "File or folder is not accessible. Cannot delete the file or folder older than 24 hours."
